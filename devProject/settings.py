b23c9733ea2fb42584a092adeac12044485b767d
zhuzhu-suda <2024m804@hokuriku-u.ac.jp>
2026-01-12 21:51:51 +0800

    Fix ALLOWED_HOSTS domain

--- devProject/settings.py
+++ devProject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = [
-    "testproject-ptxu.onrender.com",
+    "testproject-ogu0.onrender.com",
     "localhost",
     "127.0.0.1"
 ]
