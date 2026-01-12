2359399aba42e3761c9ea50d60b1b3829714b7f1
zhuzhu-suda <2024m804@hokuriku-u.ac.jp>
2026-01-12 21:45:02 +0800

    Update ALLOWED_HOSTS for Render

--- devProject/settings.py
+++ devProject/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = ["localhost","127.0.0.1"]
+ALLOWED_HOSTS = [
+    "testproject-ptxu.onrender.com",
+    "localhost",
+    "127.0.0.1"
+]
 
 
 # Application definition
