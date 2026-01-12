040cdd2bb5e1d5e5fd33259a73de5a3397ee04c3
zhuzhu-suda <2024m804@hokuriku-u.ac.jp>
2026-01-12 22:09:12 +0800

    Fix home_view route

--- devProject/urls.py
+++ devProject/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
+def home_view(request):
+    return HttpResponse("<h1>Deployment Successful!</h1><p>Django is running on Render.</p>")
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", home_view),
     path("test/",include('testApp.urls')),
 ]
